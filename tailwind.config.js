5319864e82c36e25212f4af40c07e6b310310baf
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-05-03 06:37:37 +0330

    Added new extrablack for Recursive font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ import colors from "tailwindcss/colors"
 export default {
     content: ["./src/**/*.{html,js,svelte,ts}"],
     theme: {
+        extend: {
+            fontWeight: {
+                extrablack: 1000,
+            },
+        },
         colors: {
             gray: colors.zinc,
             primary: {
