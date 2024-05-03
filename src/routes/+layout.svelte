92593fc7c9351d5a44d9b13763c2c0a47cce3f9a
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-05-03 10:46:03 +0330

    Removed Floating Blob for now!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
     import "../app.pcss"
-    import FloatingBlob from "$lib/components/FloatingBlob.svelte"
 </script>
-
-<FloatingBlob />
 
 <div class="container min-h-screen grid items-start">
     <slot />
