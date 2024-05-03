02404d0c95212a40683906a7f1cdf8fdbbf76204
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-05-03 06:30:45 +0330

    Added fonts. Changed default Tailwind colors. Added container. Added default title. Added base body styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,23 @@
+import colors from "tailwindcss/colors"
+
 /** @type {import("tailwindcss").Config} */
 export default {
     content: ["./src/**/*.{html,js,svelte,ts}"],
+    theme: {
+        colors: {
+            gray: colors.zinc,
+            primary: {
+                DEFAULT: colors.amber[500],
+                light: colors.amber[400],
+            },
+        },
+        fontFamily: {
+            sans: ["Recursive"],
+            bubblegum: ["Bubblegum Sans"],
+        },
+        container: {
+            center: true,
+            padding: "1rem",
+        },
+    },
 }
