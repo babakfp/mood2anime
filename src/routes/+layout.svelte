5373bcc2fa3998e71fbcd263031001a259c86e23
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-05-03 09:13:39 +0330

    Added Floating Blob

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     import "../app.pcss"
+    import FloatingBlob from "$lib/components/FloatingBlob.svelte"
 </script>
+
+<FloatingBlob />
 
 <div class="container min-h-screen grid items-start">
     <slot />
