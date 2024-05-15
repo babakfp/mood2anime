9f0f02f29e3e1cfbc6650270ebdaa046a9ea93c8
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-05-15 21:49:35 +0330

    Added loading bar for page navigations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
     import "@fontsource/bubblegum-sans"
     import "@fontsource-variable/recursive/full.css"
     import "../app.pcss"
+
+    import { LoadingBar } from "svelte-loading-bar"
 </script>
+
+<LoadingBar />
 
 <slot />
